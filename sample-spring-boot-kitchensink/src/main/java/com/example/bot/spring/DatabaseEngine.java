b9a8226ec3f518d1e57131b29bbd218a84a643bf
Steve <chenyaupun@outlook.com>
2017-09-18 16:05:33 +0800

    Test #2

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
@@ -29,16 +29,19 @@ public class DatabaseEngine {
 		BufferedReader br = null;
 		InputStreamReader isr = null;
 		try {
+			String[] textparts = text.split(":");
 			isr = new InputStreamReader(
                     this.getClass().getResourceAsStream(FILENAME));
 			br = new BufferedReader(isr);
 			String sCurrentLine;
-			
-			while (result == null && (sCurrentLine = br.readLine()) != null) {
-				String[] parts = sCurrentLine.split(":");
-				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
-					result = parts[1];
+			for (int i = 0; i < textparts.length; i++) {
+				while (result == null && (sCurrentLine = br.readLine()) != null) {
+					String[] parts = sCurrentLine.split(":");
+					if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+						result = parts[1];
+					}
 				}
+				if (result != null) break;
 			}
 		} catch (IOException e) {
 			log.info("IOException while reading file: {}", e.toString());
